a513567259e9aa16043223ee5d93ddf4d307be7b
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-08-26 03:07:50 +0300

    Add function to automatically name useState setter after state name.

--- nvim/.config/nvim/snippets/javascript.lua
+++ nvim/.config/nvim/snippets/javascript.lua
@@ -3,11 +3,14 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
--- local f = ls.function_node
+local f = ls.function_node
 -- local d = ls.dynamic_node
 -- local sn = ls.snippet_node
 
 local fmt = require("luasnip.extras.fmt").fmt
+local function firstToUpper(str)
+  return (str:gsub("^%l", string.upper))
+end
 
 return {
   s("arf",
@@ -45,8 +48,10 @@ return {
       "const [{}, {}] = {}",
       {
         i(1, "stateName"),
-        i(2, "setterName"),
-        i(3, "useState()"),
+        f(function(args)
+          return "set" .. firstToUpper(args[1][1])
+        end, { 1 }),
+        i(2, "useState('')"),
       }
     )
   ),
