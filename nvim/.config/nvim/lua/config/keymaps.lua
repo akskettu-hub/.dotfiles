3a50d37f2079ed25a47331f4bd7ea52b8ef3eb85
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-08-29 03:56:29 +0300

    Add keymaps for centering screen on search and moving by half-page. Also keymap for inserting empty line without entering insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+-- Insert empty line and return to original line
+vim.keymap.set("n", "<leader>o", "o<ESC>k", { silent = true })
+vim.keymap.set("n", "<leader>O", "O<ESC>j", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "n", "nzz", { silent = true })
+vim.keymap.set("n", "N", "Nzz", { silent = true })
 
 local ls = require("luasnip")
 
