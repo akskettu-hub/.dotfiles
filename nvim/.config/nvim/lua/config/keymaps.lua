b97506940c736360d4334dfbaefe7d923f588f77
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-08-29 03:57:52 +0300

    Forgot to save before previous commit.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
 -- Insert empty line and return to original line
 vim.keymap.set("n", "<leader>o", "o<ESC>k", { silent = true })
 vim.keymap.set("n", "<leader>O", "O<ESC>j", { silent = true })
+
+-- Centre screen on <C-d> and <C-u>
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+
+-- Centre screen on cycling through search
 vim.keymap.set("n", "n", "nzz", { silent = true })
 vim.keymap.set("n", "N", "Nzz", { silent = true })
 
